8480b15f0d2ce385ef23dd975a0ae3f76468f0ff
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-08-21 16:13:20 +0200

    feat(javascript): add logger function

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -2,8 +2,13 @@ function Kestra() {
 
 }
 
+Kestra.format = (map) => {
+    return "::" + JSON.stringify(map) + "::";
+}
+
+
 Kestra._send = (map) => {
-    console.log("::" + JSON.stringify(map) + "::");
+    console.log(Kestra.format(map));
 }
 
 Kestra._metrics = (name, type, value, tags) => {
@@ -37,6 +42,44 @@ Kestra.timer = (name, duration, tags) => {
         });
     } else {
         Kestra._metrics(name, "timer", duration, tags)
+    }
+}
+
+Kestra.logger = () => {
+    return new Logger();
+}
+
+class Logger {
+    _log(level, message) {
+        return {
+            "logs": (message instanceof Array ? message : [message])
+                .map(value => {
+                    return {
+                        "level": level,
+                        "message": value,
+                    }
+                })
+        };
+    };
+
+    trace(...message) {
+        console.trace(Kestra.format(this._log("TRACE", message)));
+    }
+
+    debug(...message) {
+        console.debug(Kestra.format(this._log("DEBUG", message)));
+    }
+
+    info(...message) {
+        console.info(Kestra.format(this._log("INFO", message)));
+    }
+
+    warn(...message) {
+        console.warn(Kestra.format(this._log("WARN", message)));
+    }
+
+    error(...message) {
+        console.error(Kestra.format(this._log("ERROR", message)));
     }
 }
 
