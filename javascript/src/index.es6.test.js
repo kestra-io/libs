721a94107bee1d6427523f3a39e249f4d472dc1a
FullStackIndie <johnmurphy_21@yahoo.com>
2024-10-05 17:16:06 -0700

    fix: refactored index.js to es6 syntax as seperate file to keep CommonJS support. also modified the Jest test for it in seperate file.

---
+++ javascript/src/index.es6.test.js
@@ -0,0 +1,36 @@
+import Logger from "./logger.js";
+import {jest} from "@jest/globals";
+
+const logger = new Logger();
+describe("Logger", () => {
+  it.each([["TRACE"], ["DEBUG"], ["INFO"], ["WARN"], ["ERROR"]])(
+    "Simple %s",
+    (type) => {
+      const logSpy = jest.spyOn(global.console, type.toLowerCase());
+
+      const val = Math.random();
+      logger.log(type, (val));
+
+      expect(logSpy).toHaveBeenCalled();
+      expect(logSpy).toHaveBeenCalledTimes(1);
+      expect(logSpy.mock.calls[0][0].indexOf(type)).toBeGreaterThan(1);
+      expect(logSpy.mock.calls[0][0].indexOf(val)).toBeGreaterThan(1);
+
+      logSpy.mockRestore();
+    }
+  );
+
+  it("Multiple INFO", () => {
+    const logSpy = jest.spyOn(global.console, "info");
+
+      logger.info(["test1", "test2"]);
+
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy.mock.calls[0][0].indexOf("INFO")).toBeGreaterThan(1);
+    expect(logSpy.mock.calls[0][0].indexOf("test1")).toBeGreaterThan(1);
+    expect(logSpy.mock.calls[0][0].indexOf("test2")).toBeGreaterThan(1);
+
+    logSpy.mockRestore();
+  });
+});
